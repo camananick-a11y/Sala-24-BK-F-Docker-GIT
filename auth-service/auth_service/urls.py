2b8315d01e7badfe018b66e1260892578775ad21
camananick-a11y <camananick@gmail.com>
2025-10-26 22:09:03 -0500

    Actualizar configuracion y estructura de auth-service

--- auth-service/auth_service/urls.py
+++ auth-service/auth_service/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Auth Service funcionando correctamente con Django")
 
 urlpatterns = [
-    path('', home),
-    path('admin/', admin.site.urls),
+    path('', home),  # Ruta principal
+    path('admin/', admin.site.urls),  # Panel de administración
+    path('api/', include('users.urls')),  # 👈 Incluye las rutas de la app 'users'
 ]
